ab8faaf7d0ec32fd53361ac9b16b385f120be4b1
Nick Owen <nowen@scottlogic.com>
2015-10-19 13:09:42 +0100

    Refactor responsibilities

--- js/data-service.js
+++ js/data-service.js
@@ -33,40 +33,23 @@
       }
 
       function process(data, date) {
-        var stepSize = 6;
         var processed = [];
-        var results = [];
 
         for (var i = 0, max = data.length; i < max; i += 1) {
-          processed.push({ time: data[i].Time, temp: +data[i].Temp });
 
-          if (processed.length % stepSize === 0) {
-            var startIndex = i - stepSize + 1;
-            var endIndex = i;
-
-            var tempArray = processed
-              .slice(startIndex, endIndex + 1)
-              .map(function (p) { return p.temp; });
-
-            var startTime = processed[startIndex].time;
-            var endTime = processed[endIndex].time;
-            var initialTemp = processed[startIndex].temp;
-            var exitTemp = processed[endIndex].temp;
-            var minTemp = Math.min.apply(Math, tempArray);
-            var maxTemp = Math.max.apply(Math, tempArray);
-
-            results.push({
-              startDate: new Date(date + 'T' + startTime),
-              endDate: new Date(date + 'T' + endTime),
-              startTemp: initialTemp,
-              endTemp: exitTemp,
-              min: minTemp,
-              max: maxTemp
-            });
-          }
+          // Use 'Z' to specify UTC time. This is incorrect for this data,
+          // but produces a better chart.
+          processed.push({
+            date: new Date(date + 'T' + data[i].Time + 'Z'),
+            temperature: +data[i].Temp,
+            humidity: +data[i].Humid,
+            dewPoint: +data[i].DewPt,
+            pressure: +data[i].Press,
+            windSpeed: +data[i].WindSp
+          });
         }
 
-        return results;
+        return processed;
       }
 
       return {
--- js/weather-controller.js
+++ js/weather-controller.js
@@ -7,7 +7,41 @@
       self.data = [];
 
       weatherService.getData().then(function (data) {
-        self.data = data;
+        self.data = process(data);
       });
+
+      var process = function (data) {
+        var result = [];
+        var stepSize = 4;
+
+        for (var i = 0, max = data.length; i < max; i += 1) {
+          var length = i + 1;
+
+          if (length % stepSize === 0) {
+            var startIndex = length - stepSize;
+            var endIndex = i;
+
+            var tempArray = data
+              .slice(startIndex, endIndex + 1)
+              .map(function (p) { return p.temperature; });
+
+            var startTime = data[startIndex].time;
+            var initialTemp = data[startIndex].temperature;
+            var exitTemp = data[endIndex].temperature;
+            var minTemp = Math.min.apply(Math, tempArray);
+            var maxTemp = Math.max.apply(Math, tempArray);
+
+            result.push({
+              date: data[startIndex].date,
+              open: initialTemp,
+              close: exitTemp,
+              low: minTemp,
+              high: maxTemp
+            });
+          }
+        }
+
+        return result;
+      };
     }]);
 }());
