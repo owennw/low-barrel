aefc5fb08acdb27e7356d8784a98d74f2291e94b
Nick Owen <nowen@scottlogic.com>
2015-10-19 13:09:22 +0100

    Display candlestick chart

--- js/lowBarrel-directive.js
+++ js/lowBarrel-directive.js
@@ -9,8 +9,29 @@
           data: '='
         },
         link: function (scope, element, attrs) {
+          var margin = { top: 20, right: 20, bottom: 20, left: 20 },
+            width = 1200 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
+
+          var chart = d3.select(element[0]).append('svg')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom);
 
           var render = function (data) {
+            var graph = fc.chart.linearTimeSeries()
+              .xDomain(fc.util.extent(data, 'date'))
+              .yDomain(fc.util.extent(data, ['high', 'low']));
+
+            var gridlines = fc.annotation.gridline();
+            var candlestick = fc.series.candlestick();
+
+            var multi = fc.series.multi()
+              .series([gridlines, candlestick]);
+            graph.plotArea(multi);
+
+            chart
+              .datum(data)
+              .call(graph);
           };
 
           scope.$watch('data', function () {
